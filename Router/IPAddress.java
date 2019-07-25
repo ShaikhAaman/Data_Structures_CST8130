f8d4d471614677162c65cba506c1aebc4450d30c
ShaikhAaman <51033370+ShaikhAaman@users.noreply.github.com>
2019-07-25 09:40:55 -0400

    Update IPAddress.java

--- Router/IPAddress.java
+++ Router/IPAddress.java
@@ -3,7 +3,7 @@ import java.util.*;
 /*****************************************************************************************
  * class IPAddress 
  * Purpose: This class represents the four octets and subnet mask of an IP address 
- * Author: Linda Crane  
+ * Author: Mohammedaaman Shaikh  
  * data members: 
  *   address - array of four ints for each octet 
  *   subnet - number of network bits (CIDR notation) 
